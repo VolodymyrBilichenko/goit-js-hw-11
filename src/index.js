a1b9146544ecaeb28cafca49ab50a1dc52a066f2
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-06-10 18:30:32 +0300

    uppdate
    index.js - add refs
    stylesearch.scss - add color text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './js/search'  
+import './js/search'
+
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    serchInp: document.querySelector('.search-inp'),
+    searchBtn: document.querySelector('.search-sub'),
+    gallery: document.querySelector('.gallery'),
+    moreBtn: document.querySelector('.load-more'),
+}
+
