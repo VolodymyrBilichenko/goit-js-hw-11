0dfb833aa8dd936570abdfd42957d08483b9d7b2
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-06-14 16:53:52 +0300

    Update news-pixabay.js
    update base_url

--- src/js/news-pixabay.js
+++ src/js/news-pixabay.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const MY_API_KEY = '37178508-b1640183771b73716106d46c3';
-const BASE_URL = 'https://pixabay.com/api/123';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export default class NewsApiService { // сервіс відповідає за запитом та за номером группи
     constructor() {
